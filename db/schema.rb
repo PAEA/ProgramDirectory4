5c4d15c02fdea14e5bf99e435ca0012f858c0115
Zeina Makky <zmakky@paeaonline.org>
2017-05-19 09:55:16 -0400

    styling the location for each school card

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170518134410) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "data_table_config_id"
-    t.string   "category",             limit: 300
+    t.string   "category",             limit: 500
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
   end
