838a966244837e96209b43257e5234903b2b0133
Luis Martinez <lmartinez@paeaonline.org>
2017-06-20 16:19:05 +0000

    Tables filters fix (Residency Requirements)

--- app/models/data_table.rb
+++ app/models/data_table.rb
@@ -2,7 +2,7 @@ class DataTable < ApplicationRecord
 
   def self.select_tables_by_filter( filter_field )
     find_by_sql("
-      SELECT distinct categories.category as field_value, programs.program
+      SELECT distinct categories.category as field_value
         FROM data_tables, main_headers, categories, data_table_configs, table_names, programs
         WHERE table_names.table_name = '" + filter_field + "'
           AND data_table_configs.table_name_id = table_names.id
@@ -11,7 +11,8 @@ class DataTable < ApplicationRecord
           AND data_tables.header_id = main_headers.id
           AND main_headers.table_name_id = table_names.id
           AND data_table_configs.id = data_tables.data_table_config_id
-          AND data_tables.program_id = programs.id order by programs.program")
+          AND data_tables.program_id = programs.id
+        ORDER BY programs.program")
   end
 
   def self.select_table_config_by_program_id( id, table_configuration_id )
