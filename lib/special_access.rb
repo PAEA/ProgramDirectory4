2bb96fc6a601f515de91e7dc4ae333ba690d336f
Luis Martinez <lmartinez@paeaonline.org>
2017-07-05 17:45:34 +0000

    Sending URL encoded password to AA for authentication

--- lib/special_access.rb
+++ lib/special_access.rb
@@ -1,3 +1,5 @@
+require 'cgi'
+
 module AuthenticateMe
   def new
   end
@@ -6,10 +8,10 @@ module AuthenticateMe
 
     url = 'https://access.adea.org/paeadev/CENSSAWEBSVCLIB.AUTHENTICATION'
     login = params['login'].to_s.strip
-    passw = params['password'].to_s
+    passw = CGI.escape(params['password'].to_s)
     data = 'p_input_xml_doc=<?xml version="1.0" encoding="UTF-8" ?><authentication-request><integratorUsername>paea</integratorUsername><integratorPassword>Authnt1c8n</integratorPassword><cust-id></cust-id><last-nm></last-nm><alias></alias><username>' + login + '</username><password><![CDATA[' + passw + ']]></password><session-id></session-id></authentication-request>'
     c = Curl::Easy.http_post(url, data) do |curl|
-      curl.headers['Content-Type'] = 'multipart/form-data'
+      curl.headers['Content-Type'] = 'application/x-www-form-urlencoded'
     end
     xml_response = Nokogiri::XML(c.body_str)
     authentication = xml_response.search('authentication').map do |user|
