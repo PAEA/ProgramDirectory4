516ddb3e74aa38fc78dfd5b65816dd096e455f92
Luis Martinez <lmartinez@paeaonline.org>
2018-02-22 21:23:41 +0000

    Master branch merged

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
   get "/information/:id/:edit" => "programs#information_edit"
   get "/search" => "programs#search"
   post "/authenticate" => "user_sessions#authenticate"
-<<<<<<< HEAD
   get "/terms-and-conditions" => "user_sessions#terms_and_conditions_pdf"
-=======
   get "settings" => "settings#index"
   post "/save_settings" => "settings#save_settings"
   get "/terms-and-conditions" => "user_sessions#terms_and_conditions_pdf"
@@ -19,7 +17,6 @@ Rails.application.routes.draw do
   get "/approve_change/:program_id/:field_id/:field_type" => "programs#approve_change"
   get "/reject_change/:program_id/:field_id/:field_type" => "programs#reject_change"
   post "/role_x_fields/:role_id" => "settings#role_x_fields"
->>>>>>> settings
 
   #resources :users, only: [:new, :create]
   resources :user_sessions, only: [:new, :authenticate, :destroy]
